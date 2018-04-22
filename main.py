e63964fadeb0a11c0455057b500791a594194565
Han jaeseung <wotmd5731@naver.com>
2018-04-23 02:35:01 +0900

    dd

--- main.py
+++ main.py
@@ -26,10 +26,21 @@ def data_preprocess(dir_path):
 #                total_data.append(d)
 #                break
         [total_data.append(d) for d in rdr if 'FAX' in d[0]]
-        total_data = list(set(total_data))
-    print(total_data)
+#        total_data = list(set(total_data))
     
-data_preprocess('2017data')
+#    print(total_data)
+    return total_data
+    
+data = data_preprocess('2017data')
+sdata = sorted(data, key=lambda x: time.mktime(time.strptime(x[1],"%Y-%m-%d")))
+#[da for i, da in reversed(enumerate(sdata)) if sdata[i][1]!=sdata[i+1][1]  ]
+
+#sdata = sorted(data, key=lambda kk: data[1])
+#
+#timestamps = ['2011-06-2', '2011-08-05', '2011-02-04', '2010-1-14', '2010-12-13', '2010-1-12', '2010-2-11', '2010-2-07', '2010-12-02', '2011-11-30', '2010-11-26', '2010-11-23', '2010-11-22', '2010-11-16']
+#timestamps.sort(key=lambda x: time.mktime(time.strptime(x,"%Y-%m-%d")))
+
+
 #f = open(data_path,'r')
 
 
