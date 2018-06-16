d4703dd4b1b7ad46d1d313eed0d935a2fb5245ce
jaeshan <wotmd5731@naver.com>
2018-06-16 15:10:30 +0900

    edit_ more effic

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+import datetime
+from dateutil import parser
 import os
 import csv
 import matplotlib.pyplot as plt
@@ -31,8 +33,40 @@ def data_preprocess(dir_path):
 #    print(total_data)
     return total_data
     
-data = data_preprocess('2017data')
-sdata = sorted(data, key=lambda x: time.mktime(time.strptime(x[1],"%Y-%m-%d")))
+
+def data_pre_pro_walk(dir_path, key):
+    total_data = []
+    for (paths, dirs, files) in os.walk(dir_path):
+        for fs in files:
+            if fs == 'prices.csv':
+#                print(paths,fs)
+                with open(paths+'/'+fs,'r') as file:
+                    rdr = csv.reader(file)
+#                    [total_data.append(d) for d in rdr if key in d[0]]
+                    for da in [d for d in rdr if key in d[0]]:
+                        da.extend([parser.parse(da[1]).weekday()])
+                        total_data.append(da)
+#                        print(da)
+                    
+    np_sdata = np.array(total_data)
+    #np_sdata[:,1] is means the date
+    # following command applies unique to the date!
+    # unique  is  always sorted
+    uni_np, indic = np.unique(np_sdata[:,1],return_index=True)
+    
+#    print(np_sdata[indic])
+#    print(uni_np)
+#sdata_sorted = sorted(sdata,key=lambda x: time.mktime(time.strptime(x[1],"%Y-%m-%d")))
+    return np_sdata[indic]
+#data = data_preprocess('2017data')
+#sdata = sorted(data, key=lambda x: time.mktime(time.strptime(x[1],"%Y-%m-%d")))
+
+
+sdata = data_pre_pro_walk('2017data','FAX')
+
+
+
+
 #[da for i, da in reversed(enumerate(sdata)) if sdata[i][1]!=sdata[i+1][1]  ]
 
 #sdata = sorted(data, key=lambda kk: data[1])
@@ -42,7 +76,7 @@ sdata = sorted(data, key=lambda x: time.mktime(time.strptime(x[1],"%Y-%m-%d")))
 
 
 #f = open(data_path,'r')
-
+ 
 
 #f = open('data.csv','r',encoding='utf-8')
 #rdr = csv.reader(f)
