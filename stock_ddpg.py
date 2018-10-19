3dc04744d55dc0beb1ffbd2984dbe7e6ed438511
Han jaeseung <wotmd5731@naver.com>
2018-10-20 01:07:59 +0900

    add ddpg

--- stock_ddpg.py
+++ stock_ddpg.py
@@ -15,7 +15,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
+import visdom
 
+vis = visdom.Visdom()
 
 # In[2]:
 
@@ -45,16 +47,44 @@ class ReplayBuffer:
         self.buffer = []
         self.position = 0
     
+#    def push(self,mem):
+#        if len(self.buffer) < self.capacity:
+#            self.buffer.append(None)
+#        self.buffer[self.position] = mem
+#        self.position = (self.position + 1) % self.capacity
+#    
+    
     def push(self, state, action, reward, next_state, done):
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
         self.buffer[self.position] = (state, action, reward, next_state, done)
         self.position = (self.position + 1) % self.capacity
     
-    def sample(self, batch_size):
+    def sample(self, batch_size,seq_size=1):
         batch = random.sample(self.buffer, batch_size)
         state, action, reward, next_state, done = map(np.stack, zip(*batch))
         return state, action, reward, next_state, done
+#        batch_data = random.sample(self.buffer,batch_size)
+#        state, action, reward, next_state, done = [],[],[],[],[]
+#        for data in batch_data:
+#            start = random.randint(0,len(data)-seq_size)
+#            aa = data[start:start+seq_size]
+#            t0,t1,t2,t3,t4 = list(zip(*aa))
+#            
+#            state.append(torch.stack(t0,dim=1))
+#            action.append(torch.stack(t1,dim=1))
+#            reward.append(torch.stack(t2,dim=0))
+#            next_state.append(torch.stack(t3,dim=1))
+#            done.append(torch.tensor(t4))
+#        
+#        state  = torch.cat(state).reshape((batch_size,seq_size,-1)).to(device)
+#        action = torch.cat(action).reshape((batch_size,seq_size,-1)).to(device)
+#        reward = torch.cat(reward).reshape((batch_size,seq_size,-1)).to(device)
+#        next_state = torch.cat(next_state).reshape((batch_size,seq_size,-1)).to(device)
+#        done = torch.cat(done).reshape((batch_size,seq_size,-1)).to(device)
+#        return state, action, reward, next_state, done
+    
+    
     
     def __len__(self):
         return len(self.buffer)
@@ -102,7 +132,7 @@ class OUNoise(object):
         self.min_sigma    = min_sigma
         self.decay_period = decay_period
         self.action_dim   = action_space
-        self.low          = 0
+        self.low          = -1
         self.high         = 1
         self.reset()
         
@@ -116,9 +146,9 @@ class OUNoise(object):
         return self.state
     
     def get_action(self, action, t=0):
-        ou_state = self.evolve_state()
+        ou_state = torch.from_numpy(self.evolve_state()).float()
         self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
-        return np.clip(action + ou_state, self.low, self.high)
+        return torch.clamp(action + ou_state, self.low, self.high)
     
 #https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/ou_strategy.py
 
@@ -174,13 +204,13 @@ class PolicyNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        x = F.sigmoid(self.linear3(x))
+        x = torch.tanh(self.linear3(x))
         return x
     
     def get_action(self, state):
 #        state  = torch.FloatTensor(state).unsqueeze(0).to(device)
         action = self.forward(state.to(device))
-        return action.detach().cpu().numpy()[0, 0]
+        return action.detach().cpu()
 
 
 # <h2>DDPG Update</h2>
@@ -198,7 +228,7 @@ def ddpg_update(batch_size,
     
     state      = torch.FloatTensor(state).reshape((batch_size,-1)).to(device)
     next_state = torch.FloatTensor(next_state).reshape((batch_size,-1)).to(device)
-    action     = torch.FloatTensor(action).to(device)
+    action     = torch.FloatTensor(action).reshape((batch_size,-1)).to(device)
     reward     = torch.FloatTensor(reward).unsqueeze(1).to(device)
     done       = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
 
@@ -260,15 +290,10 @@ def random_date(start, end):
     
 class env_stock():
     def __init__(self,init_money,quantize):
-        self.count_max= 200
-        
-#        self.start_date = '2010-03-01'
-#        self.end_date = '2011-03-04'
-
+        self.count_max= 400
+        self.view_seq = 10
         self.win1= vis.line(Y=torch.Tensor([0]))
         self.win2= vis.line(Y=torch.Tensor([0]))
-
-        
         self.init_money = init_money
         self.quantize = quantize
         
@@ -286,9 +311,9 @@ class env_stock():
         training_data = data_manager.build_training_data(prep_data)
         
         # 기간 필터링
-        start = random.randint(0,(len(training_data)-self.count_max-200))
+        start = random.randint(self.view_seq ,(len(training_data)-self.count_max-200))
         
-        training_data = training_data[start:start+self.count_max+200]
+        training_data = training_data[start-self.view_seq :start+self.count_max+200]
         
 #        training_data = training_data[(training_data['date'] >= self.start_date) &
 #                                      (training_data['date'] <= self.end_date)]
@@ -308,23 +333,26 @@ class env_stock():
         self.data = self.data/self.data.std(0)
         
         
-        return self.data[self.count].view(1,-1)
+        return self.data[self.count :self.count+self.view_seq].view(1,-1)
         pass
         
 
     def step(self,action):
         #continuouse action space
-        action = action.item()
-        self.sum_action += abs(self.prev_action - action)
-        
-        a_t = round((self.prev_action - action)*self.quantize)
         r_t = d_t= 0
+        action = action.item()
+        self.sum_action = np.clip(self.sum_action + action, 0,100)
+        aa = round(self.sum_action)
         
-        cost = self.data[self.count,1]*a_t*0.99
+        delta_a = (self.prev_action - aa)
+        
+        self.prev_action = aa
+        
+        
+        cost = self.data[self.count,1]*delta_a*0.99
         self.pocket += cost
         
-        self.prev_action = action
-        r_t = cost -self.sum_action
+        r_t = cost
 #        r_t = cost
         if self.count+1 == self.count_max:
 #            self.pocket += self.data[self.count,1]*(self.prev_action*quantize)
@@ -334,7 +362,7 @@ class env_stock():
         else:
             self.count +=1
 
-        return self.data[self.count].view(1,-1),r_t ,d_t,0
+        return self.data[self.count :self.count+self.view_seq].view(1,-1),r_t ,d_t,0
         pass
     
     def vis(self):
@@ -359,7 +387,7 @@ env.reset()
 #env = NormalizedActions(gym.make("Pendulum-v0"))
 ou_noise = OUNoise(1,theta=0.1)
 
-state_dim  = 3
+state_dim  = 3*10
 action_dim = 1
 hidden_dim = 256
 
@@ -384,8 +412,8 @@ policy_optimizer = optim.Adam(policy_net.parameters(), lr=policy_lr)
 
 
 clip = 1
-torch.nn.utils.clip_grad_norm(value_net.parameters(),clip)
-torch.nn.utils.clip_grad_norm(policy_net.parameters(),clip)
+torch.nn.utils.clip_grad_norm_(value_net.parameters(),clip)
+torch.nn.utils.clip_grad_norm_(policy_net.parameters(),clip)
 
 value_criterion = nn.MSELoss()
 
@@ -396,17 +424,16 @@ replay_buffer = ReplayBuffer(replay_buffer_size)
 # In[28]:
 
 
-max_frames  = 120000
+max_frames  = 12000000
 max_steps   = env.count_max
 frame_idx   = 0
 rewards     = []
 batch_size  = 128
+#batch_size  = 5
 
 
 # In[29]:
-import visdom
 
-vis = visdom.Visdom()
 win_p = vis.line(Y=torch.Tensor([0]),opts=dict(title='policy'))
 win_v = vis.line(Y=torch.Tensor([0]), opts=dict(title='value'))
 win_r = vis.line(Y=torch.Tensor([0]), opts=dict(title='reward'))
@@ -418,8 +445,8 @@ print(max_steps)
 
 while frame_idx < max_frames:
     state = env.reset()
-    env.vis()
     
+    mem = []
     ou_noise.reset()
     episode_reward = 0
     traj = []
@@ -427,14 +454,18 @@ while frame_idx < max_frames:
     for step in range(max_steps):
         action = policy_net.get_action(state)
         action = ou_noise.get_action(action, step)
-        traj.append(action.item())
-        next_state, reward, done, _ = env.step(action)
+        
+        traj.append(env.sum_action)
+        
+        next_state, reward, done, _ = env.step(action*5)
         
         replay_buffer.push(state, action, reward, next_state, done)
+        
         if len(replay_buffer) > batch_size and frame_idx%10 == 0:
             ploss,vloss=ddpg_update(batch_size)
-            win_p = vis.line(X=torch.Tensor([frame_idx]), Y=ploss.view(1) , win = win_p, update='append')
-            win_v = vis.line(X=torch.Tensor([frame_idx]), Y=vloss.view(1) , win = win_v,  update='append')
+            if frame_idx%1000 ==0:
+                win_p = vis.line(X=torch.Tensor([frame_idx]), Y=ploss.view(1) , win = win_p, update='append')
+                win_v = vis.line(X=torch.Tensor([frame_idx]), Y=vloss.view(1) , win = win_v,  update='append')
             
         state = next_state
         episode_reward += reward
@@ -444,8 +475,10 @@ while frame_idx < max_frames:
 #            plot(frame_idx, rewards)
             
         if done:
+#            replay_buffer.push(mem)
             win_r = vis.line(X=torch.Tensor([frame_idx]), Y=torch.Tensor([episode_reward]).view(1) , win = win_r , update='append')
             win_a = vis.line(Y=torch.Tensor(traj) , win = win_a)
+            env.vis()
             
             break
     
